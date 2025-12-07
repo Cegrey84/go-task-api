f150b0bcf719c3635ae4f7e35bccb955d3222e51
Cegrey84 <koostov@mail.ru>
2025-12-07 19:03:13 +0500

    Successfully configured .gitignore to exclude Postman collections

--- main.go
+++ main.go
@@ -3,102 +3,94 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/gorilla/mux"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type Task struct {
-	ID   int    `json:"id"`
-	Text string `json:"text"`
+	ID     uint   `json:"id" gorm:"primaryKey;autoIncrement"`
+	Text   string `json:"text"`
+	IsDone bool   `json:"is_done"`
 }
 
-var tasks []Task
-var nextID = 1
-
-func getTasks(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Only GET allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(tasks)
-}
-
-func postTask(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Only POST allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	var newTask Task
-	if err := json.NewDecoder(r.Body).Decode(&newTask); err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-	newTask.ID = nextID
-	nextID++
-	tasks = append(tasks, newTask)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(newTask)
-}
-
-func patchTask(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPatch {
-		http.Error(w, "Only PATCH allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
-		return
-	}
-	for i, task := range tasks {
-		if task.ID == id {
-			var updates map[string]interface{}
-			if err := json.NewDecoder(r.Body).Decode(&updates); err != nil {
-				http.Error(w, "Invalid JSON", http.StatusBadRequest)
-				return
-			}
-			if text, ok := updates["text"].(string); ok {
-				tasks[i].Text = text
-			}
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(tasks[i])
-			return
-		}
-	}
-	http.Error(w, "Task not found", http.StatusNotFound)
-}
-
-func deleteTask(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodDelete {
-		http.Error(w, "Only DELETE allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
-		return
-	}
-	for i, task := range tasks {
-		if task.ID == id {
-			tasks = append(tasks[:i], tasks[i+1:]...)
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-	}
-	http.Error(w, "Task not found", http.StatusNotFound)
-}
+var db *gorm.DB
 
 func main() {
+
+	var err error
+	db, err = gorm.Open(sqlite.Open("file:tasks.db?_pragma=busy_timeout(5000)"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect database:", err)
+	}
+
+	err = db.AutoMigrate(&Task{})
+	if err != nil {
+		log.Fatal("Failed to migrate:", err)
+	}
+
 	r := mux.NewRouter()
-	r.HandleFunc("/tasks", getTasks).Methods("GET")
-	r.HandleFunc("/task", postTask).Methods("POST")
-	r.HandleFunc("/task/{id}", patchTask).Methods("PATCH")
-	r.HandleFunc("/task/{id}", deleteTask).Methods("DELETE")
+
+	r.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		var tasks []Task
+		db.Find(&tasks)
+		json.NewEncoder(w).Encode(tasks)
+	}).Methods("GET")
+
+	r.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		var task Task
+		json.NewDecoder(r.Body).Decode(&task)
+
+		result := db.Create(&task)
+		if result.Error != nil {
+			http.Error(w, "DB error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(task)
+	}).Methods("POST")
+
+	r.HandleFunc("/tasks/{id}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id, _ := strconv.Atoi(vars["id"])
+
+		var task Task
+		db.First(&task, id)
+
+		var updates map[string]interface{}
+		json.NewDecoder(r.Body).Decode(&updates)
+
+		if text, ok := updates["text"].(string); ok {
+			task.Text = text
+		}
+		if isDone, ok := updates["is_done"].(bool); ok {
+			task.IsDone = isDone
+		}
+
+		db.Save(&task)
+		json.NewEncoder(w).Encode(task)
+	}).Methods("PATCH")
+
+	r.HandleFunc("/tasks/{id}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		id, _ := strconv.Atoi(vars["id"])
+
+		var task Task
+		db.First(&task, id)
+		task.IsDone = true
+		db.Save(&task)
+
+		w.WriteHeader(http.StatusNoContent)
+	}).Methods("DELETE")
+
 	fmt.Println("Server running on http://localhost:8080")
+	fmt.Println("Database: tasks.db")
+	fmt.Println("Using modernc.org/sqlite driver")
 	http.ListenAndServe(":8080", r)
 }
